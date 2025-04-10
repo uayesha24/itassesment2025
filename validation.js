2a4f2c10ad772786e1db9c06645846650c3bbc46
Ayesha Upadhyaya <uayesha24@tbc.edu.np>
2025-04-10 20:19:58 +0545

    Initial Commit - Add all project files

---
+++ validation.js
@@ -0,0 +1,29 @@
+function validateForm(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "") {
+        alert("Please enter your name.");
+        return false;
+    }
+
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zAZ]{2,4}$/;
+    if (email === "") {
+        alert("Please enter your email address.");
+        return false;
+    } else if (!email.match(emailPattern)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (message === "") {
+        alert("Please enter a message.");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
+}
